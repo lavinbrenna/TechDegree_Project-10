71ce89da986610856249dc33e0d21bc5baab65e6
Brenna Lavin <lavinbrenna@gmail.com>
2020-02-28 11:54:39 -0800

    adds empty component files and working App.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+//import routes
+import Header from './components/Header';
+import CourseIndex from './components/CourseIndex';
+import withContext from './Context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+//context variables
+const HeaderWithContext = withContext(Header);
+
+
+export default () => (
+  <Router>
+    <div>
+      <HeaderWithContext/>
+      <Switch>
+      
+      </Switch>
+      </div>
+  </Router>
+);
+
---
+++ src/components/CourseIndex.js
@@ -0,0 +1,13 @@
+//header
+
+//const 
+//div course containers 
+
+//div class = grid-33
+//a class = course--module course --link
+//h4 class = course--label 
+//h3 class = course --title {course.title}
+
+//div class = grid-33
+//a class = course--module course--add--module link to create course
+//h3 class = course--add--title
