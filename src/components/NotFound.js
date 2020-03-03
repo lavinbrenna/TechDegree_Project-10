d13d40f296312596c123d5a335a869538166b902
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2020-03-03 12:51:36 -0800

    Delete NotFound.js
    Deletes duplicate file in the wrong folder

--- src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-//applied from the React Authentication Workshop
-import React from 'react';
-
-export default () => (
-  <div className="bounds">
-    <h1>Not Found</h1>
-    <p>ლ(ಠ_ಠლ) Where did that page you were looking for go?!</p>
-  </div>
-);
