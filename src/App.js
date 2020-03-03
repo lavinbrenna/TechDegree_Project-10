87306a8943dc29b7d19904483ce446220f33aaac
Brenna Lavin <lavinbrenna@gmail.com>
2020-03-03 11:37:25 -0800

    added user sign up/sign in/sign out routes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import {
 } from 'react-router-dom';
 //import routes
 import Header from './components/Header';
-import CourseIndex from './components/CourseIndex';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import SignOut from './components/SignOut';
+//import CourseIndex from './components/CourseIndex';
 import withContext from './Context';
 
 
 //context variables
 const HeaderWithContext = withContext(Header);
+const SignInWithContext = withContext(SignIn);
+const SignUpWithContext = withContext(SignUp);
+const SignOutWithContext = withContext(SignOut);
 
 
 export default () => (
@@ -19,7 +25,9 @@ export default () => (
     <div>
       <HeaderWithContext/>
       <Switch>
-      
+      <Route path ="/signin" component ={SignInWithContext}/>
+      <Route path ="/signup" component = {SignUpWithContext}/>
+      <Route path ="/signout" component ={SignOutWithContext}/>
       </Switch>
       </div>
   </Router>
---
+++ src/components/SignOut.js
@@ -0,0 +1,10 @@
+//taken from react authentication workshop
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+export default ({context}) => {
+  context.actions.signOut();
+  return (
+    <Redirect to="/" />
+  );
+}
