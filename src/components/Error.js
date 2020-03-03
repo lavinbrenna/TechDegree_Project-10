a3f9127fa6354144829531fb3eab81ff6de212dc
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2020-03-03 12:50:25 -0800

    Delete Error.js
    Deletes duplicate file in the wrong folder

--- src/components/Error.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//creates an error page and displays a message when a non 400 error happens.
-export default () => (
-  <div className="bounds">
-    <h1>Error</h1>
-    <p>Sorry! We just encountered an error! (╯°□°)╯︵ ┻━┻ </p>
-  </div>
-);
