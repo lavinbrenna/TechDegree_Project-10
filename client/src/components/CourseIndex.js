d95dcea1e378c57277987ad0c1988b56c2effcde
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2020-03-09 16:25:06 -0700

    Add files via upload

---
+++ client/src/components/CourseIndex.js
@@ -0,0 +1,46 @@
+/*import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class CourseIndex extends Component {
+    //sets state to an empty course array
+    state={
+        courses:[]
+        }
+    //mounts the course context to the component
+    componentDidMount(){
+        const { context } = this.props;
+    }
+    //
+    context.data.getCourses()
+
+    render (){
+        
+        const authUser =context.authenticatedUser
+    
+    return(
+    <HeaderWithContext/>
+    <div className = "bounds">
+        <div className = "grid-33">
+            <Link className ="course--module course--link">
+            
+            </Link>
+        </div>
+    </div>    
+    )
+    }
+}
+
+//header
+
+//const 
+//div course containers 
+
+//div class = grid-33
+//a class = course--module course --link
+//h4 class = course--label 
+//h3 class = course --title {course.title}
+
+//div class = grid-33
+//a class = course--module course--add--module link to create course
+//h3 class = course--add--title
+*/
---
+++ client/src/components/Error.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//creates an error page and displays a message when a non 400 error happens.
+export default () => (
+  <div className="bounds">
+    <h1>Error</h1>
+    <p>Sorry! We just encountered an error! (╯°□°)╯︵ ┻━┻ </p>
+  </div>
+);
